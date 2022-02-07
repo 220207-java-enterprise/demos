3f781f6b32dcce4ce4fe26b46b5fdd2026d54eb4
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2022-02-07 16:50:59 -0600

    Initial Push to oscar-demos branch with HelloWorld.java

---
+++ chanpasong_code/java/HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+  public static void main(String[] args) {
+    if (args.length == 1) {
+      String name = args[0];
+      System.out.println("Hello, " + name + "!");
+    } else {
+      System.out.println("Hello, World!");
+    }
+  }
+}
