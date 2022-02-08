dd2976fc36de7bf1eb070a2711b423a7ed24627e
zhenying <a664330348@gmail.com>
2022-02-08 14:17:19 -0500

    create a new branch: zhenying-demos

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
             String name = args[0];
             System.out.println("Hello, " + name + "!");
         } else {
-            System.out.println("Hello, world!");
+            System.out.println("Hello, World!");
         }
     
     }
