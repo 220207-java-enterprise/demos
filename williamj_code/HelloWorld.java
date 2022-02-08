1ad5b73c727e11d0a76d925a780ca2f109d35562
wij2012 <wij2012@gmail.com>
2022-02-08 15:05:56 -0500

    helloworld

---
+++ williamj_code/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld
+{
+    public void sayHello()
+    {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args)
+    {
+        HelloWorld speaking = new HelloWorld();
+        speaking.sayHello();
+    }
+}
