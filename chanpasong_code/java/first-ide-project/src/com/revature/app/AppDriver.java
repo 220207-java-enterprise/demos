9bc7bd10914b8c92e79755ec1d0244e0597ab823
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2022-02-09 13:10:56 -0600

    First IDE Project demo with String and basic app demos

---
+++ chanpasong_code/java/first-ide-project/src/com/revature/app/AppDriver.java
@@ -0,0 +1,13 @@
+// Package declarations must be the first non-comment line, if any
+package com.revature.app;
+
+public class AppDriver {
+    public static void main(String[] args) {
+        //quick command for System.out.println() is: sout.
+        System.out.println("Hello, IntelliJ");
+
+        //Add a breakpoint to line 12 for debugging
+        int x = 10;
+        System.out.println(x);
+    }
+}
