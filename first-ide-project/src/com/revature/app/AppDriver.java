9a9e50dc00471b1deb4db07cfff63d6c72a6539b
Aidan Amato <aidanamato@comcast.net>
2022-02-09 12:38:11 -0500

    app driver demo

---
+++ first-ide-project/src/com/revature/app/AppDriver.java
@@ -0,0 +1,11 @@
+package com.revature.app;
+
+public class AppDriver {
+    public static void main(String[] args) {
+        System.out.println("Hello IntelliJ!");
+
+        int x = 10;
+        System.out.println(x);
+    }
+}
+
